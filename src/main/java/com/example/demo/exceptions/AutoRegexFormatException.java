ebf19dbb6a21a2726e18a9b4569fff34f2da7eaa
Yong Ta Park <qkrdydxk80@nate.com>
2024-03-22 15:53:05 +0900

    remove : 미사용 예외

--- src/main/java/com/example/demo/exceptions/AutoRegexFormatException.java
+++
@@ -1,7 +0,0 @@
-package com.example.demo.exceptions;
-
-public class AutoRegexFormatException extends AutoRegexException{
-    public AutoRegexFormatException(String s) {
-        super(s);
-    }
-}
