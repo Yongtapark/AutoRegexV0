33fb1a16aabae3750548e31cc9c9b208c8cb5228
Yong Ta Park <qkrdydxk80@nate.com>
2024-03-22 15:44:33 +0900

    add: 두 문자열의 길이가 다르면 예외를 발생

---
+++ src/main/java/com/example/demo/exceptions/AutoRegexDifferentLengthException.java
@@ -0,0 +1,8 @@
+package com.example.demo.exceptions;
+
+public class AutoRegexDifferentLengthException extends AutoRegexException{
+    private final static String DEFAULT_MESSAGE = "두 문자열의 길이는 동일해야 합니다. = ";
+    public AutoRegexDifferentLengthException(String s) {
+        super(DEFAULT_MESSAGE+s);
+    }
+}
