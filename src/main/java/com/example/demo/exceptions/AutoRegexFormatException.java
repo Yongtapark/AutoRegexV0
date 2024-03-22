4c2a36acf0574f560c8056c1338dead69734f94d
Yong Ta Park <qkrdydxk80@nate.com>
2024-03-22 15:44:13 +0900

    add: 예외 추가

---
+++ src/main/java/com/example/demo/exceptions/AutoRegexFormatException.java
@@ -0,0 +1,7 @@
+package com.example.demo.exceptions;
+
+public class AutoRegexFormatException extends AutoRegexException{
+    public AutoRegexFormatException(String s) {
+        super(s);
+    }
+}
