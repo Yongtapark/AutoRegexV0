9fe7d66c9284c6fdec798bff4c6e7a543f73f159
Yong Ta Park <qkrdydxk80@nate.com>
2024-03-22 15:44:53 +0900

    add: 문자열이 지정한 크기를 넘어서면 예외를 발생

---
+++ src/main/java/com/example/demo/exceptions/AutoRegexOverMaxSizeException.java
@@ -0,0 +1,8 @@
+package com.example.demo.exceptions;
+
+public class AutoRegexOverMaxSizeException extends AutoRegexException{
+    private final static String DEFAULT_MESSAGE = "최대 문자열 길이를 초과하였습니다. = ";
+    public AutoRegexOverMaxSizeException(String s) {
+        super(DEFAULT_MESSAGE+s);
+    }
+}
